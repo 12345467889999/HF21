6e72d1c36bc7d62ee39b7986330163f68c4a5f98
Mr.Kaku <1556783302@qq.com>
2025-10-27 12:47:12 +0900

    Initial commit

---
+++ src/main/java/HF21/controller/testController.java
@@ -0,0 +1,15 @@
+package HF21.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class testController {
+
+    @GetMapping("/test")
+    public String hello() {
+        return "hello,world";
+    }
+}

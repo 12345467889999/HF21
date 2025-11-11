bbcb3cefeef73b7a5329edc765cb1ece63e3e5fb
Mr.Guo <1556783302@qq.com>
2025-11-11 15:21:02 +0900

    cors 対応2

--- src/main/java/HF21/controller/testController.java
+++ src/main/java/HF21/controller/testController.java
@@ -6,6 +6,7 @@ import HF21.service.OmikujiService;
 import HF21.utils.OmikujiUtil;
 import HF21.vo.HttpResult;
 import HF21.vo.OmikujiResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import java.io.IOException;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class testController {
 
     @Resource

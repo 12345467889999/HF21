98086df4173b07a58081130b7e94a51a466644b8
Mr.Guo <1556783302@qq.com>
2025-11-21 10:58:04 +0900

    cors

--- src/main/java/HF21/controller/testController.java
+++ src/main/java/HF21/controller/testController.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5500")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class testController {
 
     @Resource
